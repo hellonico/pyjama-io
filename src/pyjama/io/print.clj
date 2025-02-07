50460c30326decfcf401a65a83106e0fb87c79ca
Nicolas Modrzyk <hellonico@gmail.com>
2025-02-07 23:51:21 +0900

    test cases and code for blog

--- src/pyjama/io/print.clj
+++ src/pyjama/io/print.clj
@@ -40,3 +40,15 @@
     (if (map? v)
       (print-nested-map k v)
       (println (format "| %-20s | %-120s |" k (truncate-text (remove-newlines (str v))))))))
+
+(defn print-markdown-table [keys data]
+  (let [trim-str #(-> % str clojure.string/trim)
+        rows (map #(map (comp trim-str %) keys) data)
+        headers (map name keys)
+        col-widths (map (fn [col] (apply max (count (name col)) (map #(count (trim-str %)) (map col data)))) keys)
+        format-row (fn [row] (str "| " (clojure.string/join " | " (map #(format (str "%-" % "s") (trim-str %2)) col-widths row)) " |"))
+        separator (str "|-" (clojure.string/join "-|-" (map #(apply str (repeat % "-")) col-widths)) "-|")]
+    (println (format-row headers))
+    (println separator)
+    (doseq [row rows] (println (format-row row)))))
+(def print-table print-markdown-table)
--- test/grid_test.clj
+++ test/grid_test.clj
@@ -2,8 +2,11 @@
   (:require [clojure.java.io :as io]
             [clojure.pprint]
             [clojure.test :refer :all]
+            [pyjama.io.core :as pyo]
             [pyjama.io.export]
             [pyjama.io.parallelmap]
+            [pyjama.io.print]
+            [pyjama.io.readers]
             [pyjama.parallel]))
 
 (deftest preload-data-test
@@ -34,21 +37,48 @@
     (while (:processing @app-state)
       (Thread/sleep 500))))
 
+
 (deftest blue-sky-test
   (->>
-    {:url "http://localhost:11432,http://localhost:11434"
+    {:url     "http://localhost:11432,http://localhost:11434"
      :models  ["llama3.1"]
      :format  {:type "integer"}
-     :pre     "This is a potential answer %s02 for this question: %s01. Give a score to the answer on a scale 1 to 100: based on how accurate it.
+     :pre     "This is a potential answer %s02 for this question: %s01.
+     Give a score to the answer on a scale 1 to 100: based on how accurate it.
        - Do not give an answer yourself.
        - No comment.
        - No explanation.
        - No extra text. "
-     :prompts [["Why is the sky blue" "The sky appears blue because of a process called Rayleigh scattering."]
-               ["Why is the sky blue" "Blue is scattered more than other colors because it travels as shorter, smaller waves."]
-               ["Why is the sky blue" "During the day the sky looks blue because it's the blue light that gets scattered the most. "]
-               ["Why is the sky blue" "Because it is Christmas. "]
+     :prompts [["Why is the sky blue"
+                "The sky appears blue because of a process called Rayleigh scattering."]
+               ["Why is the sky blue"
+                "Blue is scattered more than other colors because it travels as shorter, smaller waves."]
+               ["Why is the sky blue"
+                "During the day the sky looks blue because it's the blue light that gets scattered the most. "]
+               ["Why is the sky blue"
+                "Because it is Christmas. "]
                ]}
-    (pyjama.parallel/pgen)
-    (map #(-> [(:prompt %) (:result %) (:url %)]))
-    (clojure.pprint/pprint)))
+    (pyjama.parallel/generate)
+    (sort-by :model)                                        ; nice sorting
+    (pyjama.io.print/print-table [:prompt :result :duration-ms])))
+
+(deftest resume-test
+  (let [pdf (pyo/download-file "https://msnlabs.com/img/resume-sample.pdf")
+        resume (pyjama.io.readers/extract-text pdf)
+        job-position "financial IT engineer in Japan"]
+    (->>
+      {:url     "http://localhost:11432"
+       :models  ["llama3.1"]
+       :pre     ["This is a potential resume %s01 for this job position: %s02.
+       With this knowledge, answer as shortly as possible the question: %s03"
+                 resume
+                 job-position]
+       :system  "always answer in a very short less than 50 characters sentence"
+       :options {:num_context 15000}
+       :prompts ["is the resume relevant"
+                 "whats the average previous job length"
+                 "what is the most obvious flaw"
+                 "what are the 3 worst points of the resume"]}
+      (pyjama.parallel/generate)
+      (sort-by :model)
+      (pyjama.io.print/print-table [:prompt :result]))))
